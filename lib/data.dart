be1bc33a92e05168ab8ce3163690be7175c9d538
SamadiPour <samadipoor2@gmail.com>
2020-09-27 09:17:52 +0330

    * Add some data for widgets

---
+++ lib/data.dart
@@ -0,0 +1,145 @@
+class Artist {
+  final int followers;
+  final String id;
+  final String image;
+  final String name;
+
+  Artist({this.followers, this.id, this.image, this.name});
+}
+
+class Playlist {
+  final String description;
+  final String id;
+  final String image;
+  final String name;
+
+  Playlist({this.description, this.id, this.image, this.name});
+}
+
+final playlists = [
+  Playlist(
+    description: "You're on top of the world. Don't forget it.",
+    id: "37i9dQZF1DX4fpCWaHOned",
+    image: "https://i.scdn.co/image/ab67706f000000037e368901f39aae9d510c8fda",
+    name: "Confidence Boost",
+  ),
+  Playlist(
+    description: "Emo rap feelings for the misunderstood. Cover: Lil Peep",
+    id: "37i9dQZF1DX6xZZEgC9Ubl",
+    image: "https://i.scdn.co/image/ab67706f00000003d4dbc065c8f0054044570104",
+    name: "Tear Drop",
+  ),
+  Playlist(
+    description:
+        "Curl up in your favorite spot with some sweet, mellow tunes...",
+    id: "37i9dQZF1DX6ziVCJnEm59",
+    image: "https://i.scdn.co/image/ab67706f000000035ae7aa0454c9eafdd6505fda",
+    name: "Your Favorite Coffeehouse",
+  ),
+  Playlist(
+    description: "No dust was found after cleaning to this playlist.",
+    id: "37i9dQZF1DX5oYC8mRiXUR",
+    image: "https://i.scdn.co/image/ab67706f00000003ac021722264b0493ab2664d2",
+    name: "Cleaning Kit",
+  ),
+  Playlist(
+    description:
+        "Chill instrumental beats. Cover / Artwork: Trina Richie Hines for lōland",
+    id: "37i9dQZF1DX0SM0LYsmbMT",
+    image: "https://i.scdn.co/image/ab67706f000000031a64a7bbdb2cd2e210bbef8b",
+    name: "Jazz Vibes",
+  ),
+  Playlist(
+    description: "We hear you",
+    id: "37i9dQZF1DX59NCqCqJtoH",
+    image: "https://i.scdn.co/image/ab67706f000000035f6755807c73eb5f0effb5f0",
+    name: "idk.",
+  ),
+  Playlist(
+    description: "Set it off with these epic anthems. Only good vibes here!",
+    id: "37i9dQZF1DWYBO1MoTDhZI",
+    image: "https://i.scdn.co/image/ab67706f0000000305b8d0fdcdd6e249dd36486e",
+    name: "Good Vibes",
+  ),
+  Playlist(
+    description: "Hits to boost your mood and fill you with happiness!",
+    id: "37i9dQZF1DXdPec7aLTmlC",
+    image: "https://i.scdn.co/image/ab67706f000000035af1070c80cd50dbbb4cfa19",
+    name: "Happy Hits!",
+  ),
+  Playlist(
+    description: "All the vibes. All the feels.",
+    id: "37i9dQZF1DX7gIoKXt0gmx",
+    image: "https://i.scdn.co/image/ab67706f0000000313601d4833623a4d6b328e38",
+    name: "All The Feels",
+  ),
+  Playlist(
+    description: "Chilled R&B jams for your most relaxed moods.",
+    id: "37i9dQZF1DX2UgsUIg75Vg",
+    image: "https://i.scdn.co/image/ab67706f000000032b29b4a26e620ccd2bd2d2f8",
+    name: "Chilled R&B",
+  ),
+];
+
+final artists = [
+  Artist(
+    followers: 7732483,
+    id: "0jnsk9HBra6NMjO2oANoPY",
+    image: "https://i.scdn.co/image/2d0b5f483c7ed95b7c3c63147c3d38f9366b2c21",
+    name: "Flo Rida",
+  ),
+  Artist(
+    followers: 3113431,
+    id: "085pc2PYOi8bGKj0PNjekA",
+    image: "https://i.scdn.co/image/ffb9f989856095ea16e38a0898e424294b88903c",
+    name: "will.i.am",
+  ),
+  Artist(
+    followers: 3522365,
+    id: "3sgFRtyBnxXD5ESfmbK4dl",
+    image: "https://i.scdn.co/image/a91b72640d92fa70bfc7556d38114b3f010c5cc5",
+    name: "LMFAO",
+  ),
+  Artist(
+    followers: 1743852,
+    id: "5Y5TRrQiqgUO4S36tzjIRZ",
+    image: "https://i.scdn.co/image/71e2c7a1026d3a2397ae7ee08797a95d9a16c19f",
+    name: "Timbaland",
+  ),
+  Artist(
+    followers: 21516552,
+    id: "1Cs0zKBU1kc0i8ypK3B9ai",
+    image: "https://i.scdn.co/image/3a8476a71714e7718a41a1157c40f93abb8cc750",
+    name: "David Guetta",
+  ),
+  Artist(
+    followers: 2482652,
+    id: "2CIMQHirSU0MQqyYHq0eOx",
+    image: "https://i.scdn.co/image/00de6379faa5c8b1006fe858cea1b2916b90352b",
+    name: "deadmau5",
+  ),
+  Artist(
+    followers: 515558,
+    id: "57dN52uHvrHOxijzpIgu3E",
+    image: "https://i.scdn.co/image/2f0c6c465a83cd196e651e3d4e7625ba799a6f60",
+    name: "Ratatat",
+  ),
+  Artist(
+    followers: 18172814,
+    id: "1vCWHaC5f2uS3yhpwWbIA6",
+    image: "https://i.scdn.co/image/81b19a403109c4fe528ee3972137127b85be9519",
+    name: "Avicii",
+  ),
+  Artist(
+    followers: 5948403,
+    id: "0oSGxfWSnnOXhD2fKuz2Gy",
+    image: "https://i.scdn.co/image/0dfa45563119fd98226ec0d5cd35a80473502469",
+    name: "David Bowie",
+  ),
+  Artist(
+    followers: 600784,
+    id: "3dBVyJ7JuOMt4GE9607Qin",
+    image: "https://i.scdn.co/image/19f0a382ca74ba65eb6eae435fc37a4cf7157df9",
+    name: "T. Rex",
+  ),
+];
